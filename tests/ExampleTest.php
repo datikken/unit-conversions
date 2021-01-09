5a87f93734adb85ab24a9d718d0d119b84a53843
datikken <tikken23@gmail.com>
2021-01-09 21:37:51 +0300

    stack test added

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,18 +11,4 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
-
-    /** @test */
-    public function testPushAndPop()
-    {
-        $stack = [];
-        $this->assertSame(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(1, count($stack));
-
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
-    }
 }
