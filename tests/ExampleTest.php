a6744aa2c1061024717f1a5686e9f4ec56440696
datikken <datikken@users.noreply.github.com>
2021-01-09 18:32:44 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -19,7 +19,7 @@ class ExampleTest extends TestCase
         $this->assertSame(0, count($stack));
 
         array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame('foo', $stack[count($stack) - 1]);
         $this->assertSame(1, count($stack));
 
         $this->assertSame('foo', array_pop($stack));
